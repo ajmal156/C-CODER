70c1dc8266a118dddfe7445da00be59a7b036200
MUHAMMAD AJMAL <ma9156899@gmail.com>
2024-12-18 18:11:49 +0500

    length of the string to calculate the total char

---
+++ working_of_length.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+main()
+{
+    string str("C++ is a very intresting programming language");
+    cout<<"Length of the given string is: "<<str.length();
+    return 0;
+
+}

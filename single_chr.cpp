150396829b8a6ac7732edb423fb9dced9ee0d1d0
MUHAMMAD AJMAL <ma9156899@gmail.com>
2024-12-21 22:07:52 +0500

    finding the single character

---
+++ single_chr.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str ="Pakistan";
+    cout<<"String Contains: "<<str<<endl;
+    cout<<"Position of s is: "<<str.find('s');
+    return 0;
+    
+}

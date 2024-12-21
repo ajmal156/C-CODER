21fbf4dccf555d7b4750c245bd74e4dda72a82bf
MUHAMMAD AJMAL <ma9156899@gmail.com>
2024-12-21 22:08:57 +0500

    length of the string

---
+++ length.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string> // For std::string
+using namespace std;
+
+int main() {
+    string str = "Hello, World!";
+    int length = str.length();
+
+    cout << "The length of the string is: " << length << endl;
+
+    return 0;
+}

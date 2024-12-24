d3816bb6225ce39e8f86836f9f6635f6083239bb
MUHAMMAD AJMAL <ma9156899@gmail.com>
2024-12-24 17:51:04 +0500

    The string to initialization for the char

---
+++ initilization.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str1("Call me");
+    string str2("Send SMS");
+    string str3("OKay");
+    cout<<"First string str1 is:"<<str1<<endl;
+    cout<<"Second string str2 is:"<<str2<<endl;
+    cout<<"Third string str3 is:"<<str3<<endl;
+    return 0;
+    
+}
